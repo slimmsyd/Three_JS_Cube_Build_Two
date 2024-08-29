aba48868d9ab414a678227b1ad0e1062ed6d508d
Sydney Sanders <ssanderss444@gmail.com>
2024-08-29 04:54:53 -0400

    Updatd the cubes

--- js/Database.js
+++ js/Database.js
@@ -1,5 +1,136 @@
 export const cubeBank = {
-
+    "1": {
+        file: "1.glb",
+        icon: "1.png"
+    },
+    "2": {
+        file: "2.glb",
+        icon: "2.png"
+    },
+    "3": {
+        file: "3.glb",
+        icon: "3.png"
+    },
+    "4": {
+        file: "4.glb",
+        icon: "4.png"
+    },
+    "5": {
+        file: "5.glb",
+        icon: "5.png"
+    },
+    "6": {
+        file: "6.glb",
+        icon: "6.png"
+    },
+    "7": {
+        file: "7.glb",
+        icon: "7.png"
+    },
+    "8": {
+        file: "8.glb",
+        icon: "8.png"
+    },
+    "9": {
+        file: "9.glb",
+        icon: "9.png"
+    },
+    "10": {
+        file: "10.glb",
+        icon: "10.png"
+    },
+    "11": {
+        file: "11.glb",
+        icon: "11.png"
+    },
+    "1": {
+        file: "1.glb",
+        icon: "1.png"
+    },
+    "2": {
+        file: "2.glb",
+        icon: "2.png"
+    },
+    "3": {
+        file: "3.glb",
+        icon: "3.png"
+    },
+    "4": {
+        file: "4.glb",
+        icon: "4.png"
+    },
+    "5": {
+        file: "5.glb",
+        icon: "5.png"
+    },
+    "6": {
+        file: "6.glb",
+        icon: "6.png"
+    },
+    "7": {
+        file: "7.glb",
+        icon: "7.png"
+    },
+    "8": {
+        file: "8.glb",
+        icon: "8.png"
+    },
+    "9": {
+        file: "9.glb",
+        icon: "9.png"
+    },
+    "10": {
+        file: "10.glb",
+        icon: "10.png"
+    },
+    "11": {
+        file: "11.glb",
+        icon: "11.png"
+    },
+    "1": {
+        file: "1.glb",
+        icon: "1.png"
+    },
+    "2": {
+        file: "2.glb",
+        icon: "2.png"
+    },
+    "3": {
+        file: "3.glb",
+        icon: "3.png"
+    },
+    "4": {
+        file: "4.glb",
+        icon: "4.png"
+    },
+    "5": {
+        file: "5.glb",
+        icon: "5.png"
+    },
+    "6": {
+        file: "6.glb",
+        icon: "6.png"
+    },
+    "7": {
+        file: "7.glb",
+        icon: "7.png"
+    },
+    "8": {
+        file: "8.glb",
+        icon: "8.png"
+    },
+    "9": {
+        file: "9.glb",
+        icon: "9.png"
+    },
+    "10": {
+        file: "10.glb",
+        icon: "10.png"
+    },
+    "11": {
+        file: "11.glb",
+        icon: "11.png"
+    },
 }
 
 
