f347a402032dea85df585430dcb247b4cd0e5909
Sydney Sanders <ssanderss444@gmail.com>
2024-09-03 15:43:05 -0400

    added the cube bank

--- js/Database.js
+++ js/Database.js
@@ -48,6 +48,6 @@ export const cubeBank = {
 
 
 export function updateCubeBank(newData) {
-    // Object.assign(cubeBank, newData);
+    Object.assign(cubeBank, newData);
     console.log('Updated cubeBank:', cubeBank); // Log the updated cubeBank after population
 }
